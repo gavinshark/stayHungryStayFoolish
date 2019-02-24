4df79d2052a7a9c04f52e2f348334d23a4a091aa
gavinshark <zhugyseu@gmail.com>
2019-02-24 22:12:27 +0800

    fix the bug that webserver cannot respond web request many times.

--- Program/HTTP/TinyWebServer/VC/tinyWebServer/tinyWebServer.cpp
+++ Program/HTTP/TinyWebServer/VC/tinyWebServer/tinyWebServer.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
 		getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
 		printf("Accepted connection from (%s, %s)\n", hostname, port);
 		doit(connfd);
-		WSACleanup();
+		closesocket(connfd);
 	}
 	WSACleanup();
 }

fae0bd61dc5bb190b0440b4caa618e5a5d857ec7
Gavin Zhu <gavin_zhu@trendmicro.com>
2019-02-28 18:00:24 +0800

    [DNSQuery]add time_out logic

--- Program/HTTP/DNS/DNSQuery/DNSQuery/DNSQuery.cpp
+++ Program/HTTP/DNS/DNSQuery/DNSQuery/DNSQuery.cpp
@@ -9,12 +9,17 @@
 #include <windns.h>
 #include <Windows.h>
 #include <iostream>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <ctime>
 using namespace std;
 
 #pragma comment(lib, "Dnsapi.lib")
 #pragma comment(lib, "Ws2_32.lib")
 
 #define MAX_IP_STRING_LENGTH (128)
+#define MAX_TIMEOUT_MILLI_SECONDS (1000)
 
 typedef struct _DNS_RESULT {
     wstring strType;
@@ -27,70 +32,105 @@ int getDNSVector(const wstring & strHostName, VDNSRESULT & vDNSResult) {
     int ret = 0;
     vDNSResult.clear();
 
+    wcout << "start to query DNS for "<< strHostName << endl;
+
     PDNS_RECORD pDnsRecord = NULL;
     DNS_STATUS iDNSTATUS = DnsQuery_W(
         strHostName.c_str(), DNS_TYPE_ALL, DNS_QUERY_STANDARD, NULL, &pDnsRecord, NULL);//DNS_TYPE_ALL,DNS_TYPE_ANY,DNS_TYPE_A, DNS_TYPE_AAAA, DNS_TYPE_CNAME
-    
-    PDNS_RECORD pTempRecord = pDnsRecord;
-    TCHAR szTmp[MAX_IP_STRING_LENGTH] = {_T('0')};
-    while (pTempRecord != NULL)
+    ret = iDNSTATUS;
+    if (DNS_RCODE_NOERROR == iDNSTATUS)
     {
-        DNS_RESULT dnsResult;
-        switch (pTempRecord->wType)
+        PDNS_RECORD pTempRecord = pDnsRecord;
+        TCHAR szTmp[MAX_IP_STRING_LENGTH] = { _T('0') };
+        while (pTempRecord != NULL)
         {
-        case DNS_TYPE_A:
-            dnsResult.strType = L"IPV4";
-            InetNtopW(AF_INET, &pTempRecord->Data.A.IpAddress, szTmp, sizeof(szTmp));            
-            dnsResult.strDNSValue = szTmp;
-            break;
-        case DNS_TYPE_AAAA:
-            dnsResult.strType = L"IPV6";
-            InetNtopW(AF_INET6, &pTempRecord->Data.AAAA.Ip6Address, szTmp, sizeof(szTmp));
-            //wsprintf(szTmp, L"0x%032x", pTempRecord->Data.AAAA.Ip6Address);
-            dnsResult.strDNSValue = szTmp;
-            break;
-        case DNS_TYPE_CNAME:
-            dnsResult.strType = L"CNAME";
-            wsprintf(szTmp, L"%s", pTempRecord->Data.CNAME.pNameHost);
-            dnsResult.strDNSValue = szTmp;
-            break;
-        default:
-            wsprintf(szTmp, L"%d", pTempRecord->wType);
-            dnsResult.strType = szTmp;
-            dnsResult.strDNSValue = L"";
-            break;
-        }
-        
-        vDNSResult.push_back(dnsResult);
+            DNS_RESULT dnsResult;
+            switch (pTempRecord->wType)
+            {
+            case DNS_TYPE_A:
+                dnsResult.strType = L"IPV4";
+                InetNtopW(AF_INET, &pTempRecord->Data.A.IpAddress, szTmp, sizeof(szTmp));
+                dnsResult.strDNSValue = szTmp;
+                break;
+            case DNS_TYPE_AAAA:
+                dnsResult.strType = L"IPV6";
+                InetNtopW(AF_INET6, &pTempRecord->Data.AAAA.Ip6Address, szTmp, sizeof(szTmp));
+                //wsprintf(szTmp, L"0x%032x", pTempRecord->Data.AAAA.Ip6Address);
+                dnsResult.strDNSValue = szTmp;
+                break;
+            case DNS_TYPE_CNAME:
+                dnsResult.strType = L"CNAME";
+                wsprintf(szTmp, L"%s", pTempRecord->Data.CNAME.pNameHost);
+                dnsResult.strDNSValue = szTmp;
+                break;
+            default:
+                wsprintf(szTmp, L"%d", pTempRecord->wType);
+                dnsResult.strType = szTmp;
+                dnsResult.strDNSValue = L"";
+                break;
+            }
 
-        pTempRecord = pTempRecord->pNext;
+            vDNSResult.push_back(dnsResult);
+
+            pTempRecord = pTempRecord->pNext;
+        }
+    }
+    else
+    {
+        wprintf(L"DNS Query error, error code is %d\n", iDNSTATUS);
     }
 
     DnsRecordListFree(pDnsRecord, DnsFreeRecordList);
+
+    wcout << "End with querying DNS for " << strHostName << " with "<< vDNSResult.size() << " records"<< endl;
 
     return ret;
 }
 
 
 void displayDNSVector(const VDNSRESULT & vDNSResult) {
-    wcout << L"the DNS result is :\n";
+    wcout << L"the DNS result is"<<endl;
     for each (DNS_RESULT dnsResult in vDNSResult)
     {
-        wcout << L"wType: "<<dnsResult.strType <<L" and Resolved Address:"<< dnsResult.strDNSValue<<endl;
+        wcout << L"wType: " << dnsResult.strType << L" and Resolved Address:" << dnsResult.strDNSValue << endl;
     }
 }
 
 
+int handleDNSQuery(const wstring strHostName) {
+    VDNSRESULT vDNSResult;
+    clock_t beignTime = clock();
+    int iRet = getDNSVector(strHostName, vDNSResult);
+    clock_t endTime = clock();
+    wcout<< L"Time used(ms): " << (endTime - beignTime)<< endl;
+    if (0 == iRet)
+    {
+        displayDNSVector(vDNSResult);
+    }
+    return iRet;
+}
+
 int main(int argc, char **argv)
 {
     wstring strHostName;
-    VDNSRESULT vDNSResult;
     while (true) {
-        wcout << L"\nPlease input the HostName"<<endl;
+        wcout << L"\nPlease input the HostName" << endl;
         wcin >> strHostName;
-        if (0 == getDNSVector(strHostName, vDNSResult))
-        {
-            displayDNSVector(vDNSResult);
+
+        wcout << L"waiting..." << endl;
+        future_status status;
+
+        future<int> future_DNSQuery = async(handleDNSQuery, strHostName);
+            
+        status = future_DNSQuery.wait_for(chrono::milliseconds(MAX_TIMEOUT_MILLI_SECONDS));
+        if (future_status::deferred == status) {
+            wcout << L"deferred" << endl;
+        }
+        else if (future_status::timeout == status) {
+            wcout << L"timeout" << endl;
+        }
+        else  if (future_status::ready == status) {
+            wcout << L"finished with retCode:"<< future_DNSQuery.get() << endl;
         }
     }
     return 0;

1c5b25ce317f8fab6f5d019b274239048dd08701
Gavin Zhu <gavin_zhu@trendmicro.com>
2019-03-07 11:11:23 +0800

    add MultiLanguageCall

--- Program/MultiLanguageCall/PythonCallCPlusPlus/VCLib/VCLib/VCLib.cpp
+++ Program/MultiLanguageCall/PythonCallCPlusPlus/VCLib/VCLib/VCLib.cpp
@@ -3,27 +3,18 @@
 
 #include "stdafx.h"
 #include "stdio.h"
+#include "VCLib.h"
 // must use release version and x64
-#define EXPORT_DLL_API	extern "C" __declspec(dllexport)
-
-//1. C style function call
-EXPORT_DLL_API int add(int a, int b);
 
 int add(int a, int b) {
     return (a + b);
 }
 
 //2. class export
-class CFoo;
-class CFoo {
-public:
-    int multiply(int a, int b) {
+int CFoo:: multiply(int a, int b) {
         return (a * b);
     }
-private:
-    int m_ia;
-};
 
-EXPORT_DLL_API CFoo* CFoo_new() { wprintf(L"constructor is called.\n");return new CFoo; }
-EXPORT_DLL_API int CFoo_multiply(CFoo*pFoo, int a, int b) { return pFoo->multiply(a, b); }
-EXPORT_DLL_API void CFoo_delele(CFoo*pFoo) { wprintf(L"de-constructor is called.\n");delete pFoo; }
+CFoo* CFoo_new() { wprintf(L"constructor is called.\n");return new CFoo; }
+int CFoo_multiply(CFoo*pFoo, int a, int b) { return pFoo->multiply(a, b); }
+void CFoo_delele(CFoo*pFoo) { wprintf(L"de-constructor is called.\n");delete pFoo; }

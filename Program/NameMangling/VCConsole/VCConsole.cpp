401c9fe296c0adcad6c73aec0edc53c8a4a94760
gavinshark <zhugyseu@gmail.com>
2019-09-14 23:44:29 +0800

    add name mangling

---
+++ Program/NameMangling/VCConsole/VCConsole.cpp
@@ -0,0 +1,16 @@
+// VCConsole.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "../VCLib/VCLib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	func(1,2);
+	while(1){
+		;
+	}
+	return 0;
+}
+
---
+++ Program/NameMangling/VCLib/VCLib.h
@@ -0,0 +1,16 @@
+#ifndef VCLIB
+#define VCLIB
+
+#ifdef VCLIB_COMPILE
+#define VCLIB_IMEXPORT __declspec(dllexport)
+#else
+#define VCLIB_IMEXPORT __declspec(dllimport)
+#endif
+
+extern "C" {
+//VCLIB_IMEXPORT void func (int a, int b);
+//VCLIB_IMEXPORT void func (int a, int b=0);
+//VCLIB_IMEXPORT void func (int a, int b, int c);
+VCLIB_IMEXPORT void func (int a, int b, int c=0);
+}
+#endif

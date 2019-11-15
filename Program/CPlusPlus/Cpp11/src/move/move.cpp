fc2ef5d378c67883cd6ce80435a3f857eb90b138
Gavin Zhu <gavin_zhu@trendmicro.com>
2019-11-15 17:33:44 +0800

    c11 move

--- Program/CPlusPlus/Cpp11/src/move/move.cpp
+++ Program/CPlusPlus/Cpp11/src/move/move.cpp
@@ -10,11 +10,74 @@ int i = 101, j = 101;
 int foo() { return i; }
 int&& bar() { return std::move(i); }
 void set(int&& k) { k = 102; }
+
+
+class A {
+public:
+    A() {
+        std::cout << "A()" << std::endl;
+    }
+
+    A(int j) {
+        std::cout << "A("<< j <<")" << std::endl;
+        m_i = j;
+    }
+
+    ~A() {
+        m_i = 0;
+    }
+
+    A(const A & a) {
+        std::cout << "A(const A & a)" << std::endl;
+        m_i = a.m_i;
+    }
+
+    void display() {
+        std::cout << m_i << std::endl;
+    }
+
+    A(A && a) {
+        std::cout << "A(A && a)" << std::endl;
+        m_i = a.m_i;
+        a.m_i = 0;
+    }
+
+private:
+    int m_i;
+};
+
+A Get() {
+    std::cout << "A Get()" << std::endl;
+    return A(0);
+}
+
+
 int main()
 {
+#if 0
     foo();
     std::cout << i << std::endl;
     set(bar());
     std::cout << i << std::endl;
+#endif
+
+#if 0
+    A a(123);
+    std::cout << "a.display()" << std::endl;
+    a.display();
+    A b(std::move(a));
+    std::cout << "b.display()" << std::endl;
+    b.display();
+    std::cout << "a.display()" << std::endl;
+    a.display();
+#endif
+
+#if 0
+    //complier optimization
+    A a(A(13));
+    a.display();
+    A aa(Get());
+    aa.display();
+#endif
 }
 

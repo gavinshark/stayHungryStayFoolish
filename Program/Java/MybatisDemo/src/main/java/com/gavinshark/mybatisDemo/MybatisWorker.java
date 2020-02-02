022688ba9a99fd81eff591325a4660d532852574
gavinshark <zhugyseu@gmail.com>
2020-02-02 23:09:26 +0800

    mybatis/refine mybatis configuration

--- Program/Java/MybatisDemo/src/main/java/com/gavinshark/mybatisDemo/MybatisWorker.java
+++ Program/Java/MybatisDemo/src/main/java/com/gavinshark/mybatisDemo/MybatisWorker.java
@@ -9,16 +9,26 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.*;
 
 public class MybatisWorker {
+    private static final String resource = "mybatis-config.xml";
+    private static InputStream inputStream;
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Logger mylogger;
+    static {
+        try {
+            //1. get factory
+            mylogger = Logger.getLogger(MybatisWorker.class);
+            inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e){
+            mylogger.error("error:" + e.toString());
+        }
+    }
     public static void displaySalarybyId(Integer id) throws IOException {
-        //1. get factory
-        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         //2.get session
         try (SqlSession session = sqlSessionFactory.openSession()) {
             SalaryDao mapper = session.getMapper(SalaryDao.class);
             Salary salary = mapper.getSalaryById(id);
             System.out.println("get salary of id=" + id + ", the result is:" + salary);
-            Logger mylogger = Logger.getLogger(MybatisWorker.class.getName());
             mylogger.info("get salary of id=" + id + ", the result is:" + salary);
         }
     }

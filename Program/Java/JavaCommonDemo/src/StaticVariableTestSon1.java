1c3b67d3bca8e72e17a24f4b44ba02eecf827b20
gavinshark <zhugyseu@gmail.com>
2020-02-06 15:05:04 +0800

    add static variable test

---
+++ Program/Java/JavaCommonDemo/src/StaticVariableTestSon1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class StaticVariableTestSon1 extends StaticVariableTest{
+    protected void display(){
+        System.out.println("StaticVariableTestSon 1");
+    }
+}

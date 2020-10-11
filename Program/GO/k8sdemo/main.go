852370a06588189c43421c6a6dfdb40b69789246
Gavin Zhu <gavin_zhu@trendmicro.com>
2020-10-11 22:55:24 +0800

    refine k8s demo

--- Program/GO/k8sdemo/main.go
+++ Program/GO/k8sdemo/main.go
@@ -15,81 +15,86 @@ limitations under the License.
 package main
 
 import (
-	"context"
-	"fmt"
-	"k8s.io/client-go/util/retry"
-	"time"
+        "context"
+        "fmt"
+        "k8s.io/client-go/util/retry"
+        "time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth"
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+        "k8s.io/apimachinery/pkg/api/errors"
+        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+        "k8s.io/client-go/kubernetes"
+        "k8s.io/client-go/rest"
+        //
+        // Uncomment to load all auth plugins
+        // _ "k8s.io/client-go/plugin/pkg/client/auth"
+        //
+        // Or uncomment to load specific auth plugins
+        // _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+        // _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+        // _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+        // _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
 func main() {
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+        // creates the in-cluster config
+        config, err := rest.InClusterConfig()
+        if err != nil {
+                panic(err.Error())
+        }
+        // creates the clientset
+        clientset, err := kubernetes.NewForConfig(config)
+        if err != nil {
+                panic(err.Error())
+        }
 
-	//
-	dClient := clientset.AppsV1().Deployments("go-client")
+        time.Sleep(30 * time.Second)
+        fmt.Printf("this is version 0.10\n")
 
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		// Assumes you've already deployed nginx before to the cluster
-		result, getErr := dClient.Get(context.TODO(), "nginx-deployment", metav1.GetOptions{})
-		if getErr != nil {
-			panic(fmt.Errorf("Failed to get latest version of nginx-deployment: %s", getErr))
-		}
+        //
+        dClient := clientset.AppsV1().Deployments("go-client")
 
-		result.Spec.Template.Spec.Containers[0].Image = "nginx:1.19"
-		_, updateErr := dClient.Update(context.TODO(), result, metav1.UpdateOptions{})
-		return updateErr
-	})
+        retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+        // Assumes you've already deployed nginx before to the cluster
+                result, getErr := dClient.Get(context.TODO(), "go-update-client", metav1.GetOptions{})
+                if getErr != nil {
+                        panic(fmt.Errorf("Failed to get latest version of nginx-deployment: %s", getErr))
+                }
+//
+                //result.Spec.Template.Spec.Containers[0].Image = "gaivnshark/goupdateclient:0.8"
+                fmt.Printf("current version is %s\n", result.Spec.Template.Spec.Containers[0].Image)
+                _, updateErr := dClient.Update(context.TODO(), result, metav1.UpdateOptions{})
+                return updateErr
+        })
 
-	if retryErr != nil {
-		panic(retryErr)
-	}
-	//
+        if retryErr != nil {
+                panic(retryErr)
+        }
+        //
 
-	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		pods, err := clientset.CoreV1().Pods("go-client").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+        for {
+                // get pods in all the namespaces by omitting namespace
+                // Or specify namespace to get pods in particular namespace
+                pods, err := clientset.CoreV1().Pods("go-client").List(context.TODO(), metav1.ListOptions{})
+                if err != nil {
+                        panic(err.Error())
+                }
 
-		// Examples for error handling:
-		// - Use helper functions e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("go-client").Get(context.TODO(), "nginx-deployment-7fdb6f477d-6p564", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod nginx-deployment-7fdb6f477d-6p564 not found in go-client namespace\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found nginx-deployment-7fdb6f477d-6p564 pod in go-client namespace\n")
-		}
+                fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		time.Sleep(10 * time.Second)
-	}
+                // Examples for error handling:
+                // - Use helper functions e.g. errors.IsNotFound()
+                // - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+                _, err = clientset.CoreV1().Pods("go-client").Get(context.TODO(), "nginx-deployment-7fdb6f477d-6p564", metav1.GetOptions{})
+                if errors.IsNotFound(err) {
+                        fmt.Printf("Pod nginx-deployment-7fdb6f477d-6p564 not found in go-client namespace\n")
+                } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+                        fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+                } else if err != nil {
+                        panic(err.Error())
+                } else {
+                        fmt.Printf("Found nginx-deployment-7fdb6f477d-6p564 pod in go-client namespace\n")
+                }
+
+                time.Sleep(10 * time.Second)
+        }
 }

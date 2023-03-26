160556d6e22bbc5430999b94c68c32c7571e371e
Gavin Zhu <gavinzhuseu@foxmail.com>
2023-03-26 23:09:09 +0800

    mybatis: with  transaction

--- Program/Java/SpringBootDemo/batisdemo/batisdemo/src/main/java/com/gavin/batisdemo/demo/DemoApplication.java
+++ Program/Java/SpringBootDemo/batisdemo/batisdemo/src/main/java/com/gavin/batisdemo/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.gavin.batisdemo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
--- Program/Java/SpringBootDemo/batisdemo/batisdemo/src/main/java/com/gavin/batisdemo/demo/dao/StudentMapper.java
+++ Program/Java/SpringBootDemo/batisdemo/batisdemo/src/main/java/com/gavin/batisdemo/demo/dao/StudentMapper.java
@@ -1,7 +1,9 @@
 package com.gavin.batisdemo.demo.dao;
 
 import com.gavin.batisdemo.demo.pojo.Student;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface StudentMapper {
     Student selectStudent(Integer id);
 
